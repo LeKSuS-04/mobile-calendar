de3926bc932900572e08143ca6c8e99c2243b5ee
Alexey Tarasov <tarasovion2004@gmail.com>
2023-03-23 20:58:04 +0300

    Panels draft

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
+
+import TopPanel from "./lib/panels/TopPanel";
+import BottomPanel from "./lib/panels/BottomPanel";
 import Grid from "./lib/grid/Grid";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <TopPanel />
       <Grid width="7" height="6" />
+      <BottomPanel />
       <StatusBar style="auto" />
     </View>
   );
